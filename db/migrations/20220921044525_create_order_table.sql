d2d06d8b8ffda895134838f395cf594808f7e719
pienk <kisuk623@gmail.com>
2022-09-21 13:46:34 +0900

    [EDIT] orders 테이블 외래키 추가

--- db/migrations/20220921044525_create_order_table.sql
+++ db/migrations/20220921044525_create_order_table.sql
@@ -5,11 +5,12 @@ CREATE TABLE orders(
     product_id INT NOT NULL,
     quantity INT NOT NULL,
     price DECIMAL NOT NULL,
-    order_number INT NOT NULL,
+    order_list INT NOT NULL,
     order_status INT NOT NULL,
     create_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     CONSTRAINT order_user_fkey FOREIGN KEY (user_id) REFERENCES users (id),
     CONSTRAINT order_product_fkey FOREIGN KEY (product_id) REFERENCES products (id),
+    CONSTRAINT order_list_fkey FOREIGN KEY (order_list) REFERENCES order_list (id),
     CONSTRAINT order_user_pro_ukey UNIQUE (user_id, product_id)
 )
 -- migrate:down
