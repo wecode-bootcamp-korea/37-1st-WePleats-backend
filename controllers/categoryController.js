4661310172f313ce3d47d1817fa6447db0a880e5
Wi JungWoo <wirush4020@gmail.com>
2022-09-22 19:21:14 +0900

    카테테고리 to 제품 리스트 API V1.1

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,3 +1,34 @@
 const { categoryService } = require('../services');
 
-const 
+const mainCategories = async (req, res) => {
+    const { categoryId } = req.params
+    if (!categoryId) {
+        const error = new Error('KEY_ERROR')
+        error.statusCode = 400
+        throw error
+    }
+}
+
+const subCategories = async (req, res) => {
+    const { categoryId } = req.params
+    if (!categoryId) {
+        const error = new Error('KEY_ERROR')
+        error.statusCode = 400
+        throw error
+    }
+}
+
+const color = async (req, res) => {
+    const { colorId } = req.params
+    if (!colorId) {
+        const error = new Error('KEY_ERROR')
+        error.statusCode = 400
+        throw error
+    }
+}
+
+module.exports = {
+    mainCategories,
+    subCategories,
+    color
+}
--- routes/categoryRouter.js
+++ routes/categoryRouter.js
@@ -3,6 +3,8 @@ const { categoryController } = require('../controllers');
 
 const router = express.Router();
 
-router.get('/productslist', categoryController.category)
+router.get('/color/:colorId', categoryController.colors)
+router.get('/main/:categoryId', categoryController.mainCategories)
+router.get('/:categoryId', categoryController.subCategories)
 
 module.exports = router;
