e0db2f86e358cf76dedfdb574a1c744605cd86a8
Wi JungWoo <wirush4020@gmail.com>
2022-09-23 14:20:06 +0900

    로그인 통신 테스트

--- util/multer.js
+++ util/multer.js
@@ -1,7 +1,7 @@
 const multer = require("multer");
 const multerS3 = require("multer-s3");
 const aws = require("aws-sdk");
-aws.config.loadFromPath(__dirname + "/../config/s3.json");
+// aws.config.loadFromPath(__dirname + "/../config/s3.json");
 
 const s3 = new aws.S3();
 const upload = multer({
