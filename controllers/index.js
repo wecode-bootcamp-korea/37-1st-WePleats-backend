9be542cac4df1f28b841f76f582a26b1f7789ecd
Wi JungWoo <wirush4020@gmail.com>
2022-09-29 11:57:39 +0900

    카테고리리 to 제품 리스트 API V2.1 기능 구현 수정 및 컨플릭트 해결

--- controllers/index.js
+++ controllers/index.js
@@ -3,7 +3,6 @@ const userController = require('./userController');
 const reviewController = require("./reviewController");
 const productController = require("./productController")
 const cartController = require("./cartController")
-const categoryController = require("./categoryController")
 
 module.exports = {
     userController,
